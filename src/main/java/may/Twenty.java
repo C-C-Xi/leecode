9be4f3f963470d8eba63924d0b5707c01cfeff49
cc <154002582@qq.com>
2021-05-20 17:01:09 +0800

    diyitian

---
+++ src/main/java/may/Twenty.java
@@ -0,0 +1,32 @@
+package may;
+
+
+
+public class Twenty {
+    /**
+     * 二叉树中的最大路径和
+     * @param root
+     * @return
+     */
+    public int maxPathSum(TreeNode root) {
+
+        return 0;
+    }
+    public static void main(String[] args) {
+
+    }
+}
+
+
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
